c011f60475d4eecdde69a77066d8a8401190c354
Xander Grzywinski <xgrzywinski@microsoft.com>
2023-01-18 14:08:42 -0800

    initial docs site

---
+++ website/docusaurus.config.js
@@ -0,0 +1,77 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Copacetic',
+  url: 'https://project-copacetic.github.io',
+  baseUrl: '/copacetic/docs/',
+  onBrokenLinks: 'ignore',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  trailingSlash: false,
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'project-copacetic', // Usually your GitHub org/user name.
+  projectName: 'copacetic', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Copacetic',
+        logo: {
+          alt: 'Copa Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            href: 'https://github.com/project-copacetic/copacetic',
+            position: 'right',
+            className: 'header-github-link',
+            'aria-label': 'GitHub repository',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()}, Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
