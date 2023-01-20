85de1dcc534378428189702777dc0f6b028054e6
Xander Grzywinski <xandergrzyw@gmail.com>
2023-01-20 14:56:32 -0800

    add anonymized analytics to docs site (#16)
    Signed-off-by: Xander Grzywinski <xgrzywinski@microsoft.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -41,6 +41,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-3RC20QPKNS',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
