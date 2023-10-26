3fddb8f6e4b8248d5964acc49b67fea8c568aadc
Anubhav Gupta <mail.anubhav06@gmail.com>
2023-10-26 16:47:37 +0530

    fix CI
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- test/e2e/plugin_test.go
+++ test/e2e/plugin_test.go
@@ -17,24 +17,25 @@ func TestPlugins(t *testing.T) {
 		{
 			image:  "docker.io/library/nginx:1.23",
 			report: "./testdata/fake_report.json",
-			err:    fmt.Errorf("Error: unsupported osType FakeOS specified\n"),
+			err:    fmt.Errorf("Error: unsupported osType FakeOS specified\n"), // nolint:revive
 		},
 		{
 			image:  "docker.io/library/alpine:3.14.0",
 			report: "./testdata/invalid_report.json",
-			err:    fmt.Errorf("Error: error running scanner fake: exit status 1\n"),
+			err:    fmt.Errorf("Error: error running scanner fake: exit status 1\n"), // nolint:revive
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.image, func(t *testing.T) {
-			out, _ := runPatch(t, tc.image, tc.report)
+			out, _ := runPatch(tc.image, tc.report)
 			assert.Equal(t, tc.err, fmt.Errorf(string(out)))
 		})
 	}
 }
 
-func runPatch(t *testing.T, image, report string) ([]byte, error) {
+func runPatch(image, report string) ([]byte, error) {
+	//#nosec G204
 	cmd := exec.Command(
 		copaPath,
 		"patch",
