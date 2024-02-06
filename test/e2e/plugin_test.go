4be5888e1d51325cea51b5df8c66df504f921214
Anubhav Gupta <mail.anubhav06@gmail.com>
2024-02-07 03:02:18 +0530

    test: add plugin e2e tests (#391)
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>
    Co-authored-by: Sertaç Özercan <852750+sozercan@users.noreply.github.com>
    Signed-off-by: Xander Grzywinski <xandergrzyw@gmail.com>

---
+++ test/e2e/plugin_test.go
@@ -0,0 +1,55 @@
+package e2e
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPlugins(t *testing.T) {
+	testCases := []struct {
+		image  string
+		report string
+		err    error
+	}{
+		{
+			image:  "docker.io/library/alpine:3.14.0",
+			report: "./testdata/invalid_report.json",
+			err:    fmt.Errorf("exit status 1"),
+		},
+		{
+			image:  "docker.io/library/alpine:3.7.3",
+			report: "./testdata/valid_report.json",
+			err:    nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc // capture range variable
+		t.Run(tc.image, func(t *testing.T) {
+			t.Parallel()
+			_, err := runPatch(tc.image, tc.report)
+			if err != nil {
+				assert.Equal(t, tc.err, fmt.Errorf(err.Error()))
+			} else {
+				assert.Equal(t, tc.err, nil)
+			}
+		})
+	}
+}
+
+func runPatch(image, report string) ([]byte, error) {
+	//#nosec G204
+	cmd := exec.Command(
+		copaPath,
+		"patch",
+		"-i="+image,
+		"-r="+report,
+		"-s="+scannerPlugin,
+		"-a="+buildkitAddr,
+	)
+	out, err := cmd.CombinedOutput()
+	return out, err
+}
