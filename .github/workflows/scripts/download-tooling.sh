eef1b67a4138d7cfdd760be220351214eb398d1a
Sertac Ozercan <sozercan@gmail.com>
2024-03-18 23:07:55 +0000

    stuff
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- .github/workflows/scripts/download-tooling.sh
+++ .github/workflows/scripts/download-tooling.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
+runner=$1
+
 set -eu -o pipefail
 
-curl -sfL https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.deb -o trivy.deb
-sudo dpkg -i trivy.deb
-rm trivy.deb
+if [ -z "$runner" ]; then
+    echo "runner is not set"
+    exit 1
+fi
 
-curl -sfL https://github.com/moby/buildkit/releases/download/v${BUILDKIT_VERSION}/buildkit-v${BUILDKIT_VERSION}.linux-amd64.tar.gz -o buildkit.tar.gz
-sudo tar -zxvf buildkit.tar.gz -C /usr/local/
-rm buildkit.tar.gz
+if [ "$runner" == "ubuntu-22.04" ]; then
+    curl -sfL https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_Linux-64bit.deb -o trivy.deb
+    sudo dpkg -i trivy.deb
+    rm trivy.deb
+
+    curl -sfL https://github.com/moby/buildkit/releases/download/v${BUILDKIT_VERSION}/buildkit-v${BUILDKIT_VERSION}.linux-amd64.tar.gz -o buildkit.tar.gz
+    sudo tar -zxvf buildkit.tar.gz -C /usr/local/
+    rm buildkit.tar.gz
+
+    elif [ "$runner" == "darwin" ]; then
+    curl -sfL https://github.com/aquasecurity/trivy/releases/download/v${TRIVY_VERSION}/trivy_${TRIVY_VERSION}_macOS-ARM64.tar.gz -o trivy_${TRIVY_VERSION}_macOS-ARM64.tar.gz
+    tar -xvf trivy_${TRIVY_VERSION}_macOS-ARM64.tar.gz
+    sudo mv trivy /usr/local/bin
+
+    curl -sfL https://github.com/moby/buildkit/releases/download/v${BUILDKIT_VERSION}/buildkit-v${BUILDKIT_VERSION}.darwin-arm64.tar.gz -o buildkit.tar.gz
+    sudo tar -zxvf buildkit.tar.gz -C /usr/local/
+    rm buildkit.tar.gz
+else
+    echo "runner type is not supported"
+    exit 1
+fi
+
+
+
