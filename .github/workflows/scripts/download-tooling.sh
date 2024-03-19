8f808d559c9e311d06796e3d03dc3a9521e088ed
Sertac Ozercan <sozercan@gmail.com>
2024-03-19 00:57:36 +0000

    colima
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- .github/workflows/scripts/download-tooling.sh
+++ .github/workflows/scripts/download-tooling.sh
@@ -26,6 +26,9 @@ if [ "$runner" == "ubuntu-22.04" ]; then
     curl -sfL https://github.com/moby/buildkit/releases/download/v${BUILDKIT_VERSION}/buildkit-v${BUILDKIT_VERSION}.darwin-arm64.tar.gz -o buildkit.tar.gz
     sudo tar -zxvf buildkit.tar.gz -C /usr/local/
     rm buildkit.tar.gz
+
+    brew install colima docker
+    colima start
 else
     echo "runner type is not supported"
     exit 1
