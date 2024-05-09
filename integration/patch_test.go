cb08cf21ea7649857a396eee0274005225c606e7
ashnamehrotra <ashnamehrotra@gmail.com>
2024-05-09 13:08:58 -0700

    docker images debugging
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- integration/patch_test.go
+++ integration/patch_test.go
@@ -12,6 +12,7 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/distribution/reference"
 	"github.com/opencontainers/go-digest"
 	"github.com/stretchr/testify/assert"
@@ -213,6 +214,10 @@ func patch(t *testing.T, ref, patchedTag, path string, ignoreErrors bool, report
 
 	cmd.Env = append(cmd.Env, os.Environ()...)
 	cmd.Env = append(cmd.Env, dockerDINDAddress.env()...)
+
+	cmd2 := exec.Command("docker", "images")
+	out2, err := cmd2.CombinedOutput()
+	spew.Dump("DOCKER IMAGES", out2, err)
 
 	out, err := cmd.CombinedOutput()
 	require.NoError(t, err, string(out))
