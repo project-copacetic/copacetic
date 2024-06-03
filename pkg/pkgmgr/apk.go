9ef953d7b44c242a2da7ea3373558b72f9f768d1
ashnamehrotra <ashnamehrotra@gmail.com>
2024-06-03 14:53:02 -0700

    apk fix
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- pkg/pkgmgr/apk.go
+++ pkg/pkgmgr/apk.go
@@ -165,8 +165,12 @@ func (am *apkManager) upgradePackages(ctx context.Context, updates unversioned.U
 	// TODO: Add support for custom APK config
 	apkUpdated := am.config.ImageState.Run(llb.Shlex("apk update"), llb.WithProxy(utils.GetProxy()), llb.IgnoreCache).Root()
 
-	checkUpgradable := `sh -c "apk -u list 2>/dev/null | grep -q "upgradable" || exit 1"`
-	apkUpdated = apkUpdated.Run(llb.Shlex(checkUpgradable)).Root()
+	// If updating all packages, check for upgrades before proceeding with patch
+	if updates == nil {
+		checkUpgradable := `sh -c "apk list 2>/dev/null | grep -q "upgradable" || exit 1"`
+		apkUpdated = apkUpdated.Run(llb.Shlex(checkUpgradable)).Root()
+
+	}
 
 	var apkInstalled llb.State
 	var resultManifestBytes []byte
