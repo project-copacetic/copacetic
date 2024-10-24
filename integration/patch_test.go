83c36cc723906f23a5d7af5b4463a6ddc3e15b3a
Sertac Ozercan <sozercan@gmail.com>
2024-10-24 04:48:09 +0000

    update
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- integration/patch_test.go
+++ integration/patch_test.go
@@ -235,9 +235,10 @@ func (s *scannerCmd) scan(t *testing.T, ref string, ignoreErrors bool) {
 	args := []string{
 		"trivy",
 		"image",
-		"--vuln-type=os",
+		"--pkg-types=os",
 		"--ignore-unfixed",
 		"--scanners=vuln",
+		"--db-repository=ghcr.io/aquasecurity/trivy-db:2,public.ecr.aws/aquasecurity/trivy-db",
 	}
 	if s.output != "" {
 		args = append(args, []string{"-o=" + s.output, "-f=json"}...)
