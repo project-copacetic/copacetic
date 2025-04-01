611d00af546fa3ea932af6e9aa30e9257c46f5e8
ashnamehrotra <ashnamehrotra@gmail.com>
2025-04-01 13:56:01 -0400

    this still works
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- pkg/buildkit/buildkit.go
+++ pkg/buildkit/buildkit.go
@@ -46,16 +46,15 @@ func InitializeBuildkitConfig(ctx context.Context, c gwclient.Client, userImage 
 		return nil, err
 	}
 
-	/*
+	var baseImage string
+	config.ConfigData, config.PatchedConfigData, baseImage, err = updateImageConfigData(ctx, c, configData, userImage)
+	if err != nil {
+		return nil, err
+	}
 
-		var baseImage string
-		config.ConfigData, config.PatchedConfigData, baseImage, err = updateImageConfigData(ctx, c, configData, userImage)
-		if err != nil {
-			return nil, err
-		}
-	*/
+	fmt.Print("baseImage: ", baseImage, "\n")
 
-	config.ConfigData = configData
+	// config.ConfigData = configData
 
 	// Load the target image state with the resolved image config in case environment variable settings
 	// are necessary for running apps in the target image for updates
