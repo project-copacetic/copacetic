015657e2dc3f3f3d6b87db3294f4e48a6ca6cad9
ashnamehrotra <ashnamehrotra@gmail.com>
2025-05-01 16:34:16 -0400

    revert changes
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- pkg/pkgmgr/dpkg.go
+++ pkg/pkgmgr/dpkg.go
@@ -488,8 +488,10 @@ func (dm *dpkgManager) unpackAndMergeUpdates(ctx context.Context, updates unvers
 
 							while IFS=':' read -r package version; do
 								pkg_name=$(echo "$package" | sed 's/^"\(.*\)"$/\1/')
-								apt-get install --fix-broken --reinstall -y $pkg_name
+								apt-get install --reinstall --no-install-recommends -y $pkg_name
 							done <<< "$(echo "$json_str" | tr -d '{}\n' | tr ',' '\n')"
+
+							apt --fix-broken install
 
 							dpkg --configure -a
 							apt-get check
