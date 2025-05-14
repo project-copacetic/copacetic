e337bd50132ed85b565fa8a5d74cf7fba6d12acc
ashnamehrotra <ashnamehrotra@gmail.com>
2025-05-14 13:30:43 -0400

    merge into one script
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- pkg/pkgmgr/scripts/apt_get_download.sh
+++ pkg/pkgmgr/scripts/apt_get_download.sh
@@ -1,11 +1,19 @@
 if [ "$IGNORE_ERRORS" = "true" ]; then
-	set -x
+    set -x
 else
 	set -ex
 fi
 
-packages="%s"
+# pass map of file name to package 
+
+if ["$UPDATE_ALL" = "true"]; then
+    packages="$(cat /var/cache/apt/archives/packages.txt)"
+else
+    packages="%s"
+fi
+
 apt-get update
+
 apt-get download --no-install-recommends $packages
 dpkg --root=/tmp/debian-rootfs --admindir=/tmp/debian-rootfs/var/lib/dpkg --force-all --force-confold --install *.deb
 dpkg --root=/tmp/debian-rootfs --configure -a
@@ -20,45 +28,45 @@ package_name=""
 package_content=""
 
 while IFS= read -r line || [ -n "$line" ]; do
-	if [ -z "$line" ]; then
-		# end of a package block
-		if [ -n "$package_name" ]; then
-				# handle special case for base-files
-			if [ "$package_name" = "base-files" ]; then
-				output_name="base"
-			elif [ "$package_name" = "libssl1.1" || "$package_name" = "libssl" ]; then
+    if [ -z "$line" ]; then
+        # end of a package block
+        if [ -n "$package_name" ]; then
+            # handle special case for base-files
+            if [ "$package_name" = "base-files" ]; then
+                output_name="base"
+            elif [ "$package_name" = "libssl1.1" || "$package_name" = "libssl1" ]; then
 				output_name="libssl1.1"
-			else 
-				output_name="$package_name"
-			fi
-			# write the collected content to the package file
-			echo "$package_content" > "$OUTPUT_DIR/$output_name"
-		fi
+            else 
+                output_name="$package_name"
+            fi
+            # write the collected content to the package file
+            echo "$package_content" > "$OUTPUT_DIR/$output_name"
+        fi
 
-		# re-set for next package
-		package_name=""
-		package_content=""
-	else
-		# add current line to package content
-		if [ -z "$package_content" ]; then
-			package_content="$line"
-		else
-			package_content="$package_content
+        # re-set for next package
+        package_name=""
+        package_content=""
+    else
+        # add current line to package content
+        if [ -z "$package_content" ]; then
+            package_content="$line"
+        else
+            package_content="$package_content
 $line"
-		fi
+        fi
 
-		case "$line" in
-			"Package:"*)
-				# extract package name
-				package_name=$(echo "$line" | cut -d' ' -f2)
-				;;
-			esac
-		fi
+        case "$line" in
+            "Package:"*)
+                # extract package name
+                package_name=$(echo "$line" | cut -d' ' -f2)
+                ;;
+        esac
+    fi
 done < "$STATUS_FILE"
 
 # handle last block if file does not end with a newline
 if [ -n "$package_name" ] && [ -n "$package_content" ]; then
-	echo "$package_content" > "$OUTPUT_DIR/$package_name"
+    echo "$package_content" > "$OUTPUT_DIR/$package_name"
 fi
 
 # delete everything else inside /tmp/debian-rootfs/var/lib/dpkg except status.d
@@ -66,6 +74,6 @@ find /tmp/debian-rootfs/var/lib/dpkg -mindepth 1 -maxdepth 1 ! -name "status.d" 
 
 # write results manifest for validation
 for deb in *.deb; do
-	dpkg-deb -f "$deb" | grep "^Package:\|^Version:" >> /tmp/debian-rootfs/manifest
+    dpkg-deb -f "$deb" | grep "^Package:\|^Version:" >> /tmp/debian-rootfs/manifest
 done
 
