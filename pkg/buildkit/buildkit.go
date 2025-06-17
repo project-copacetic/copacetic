8c532ca98becd255162d7de7985fca4d5b53ea95
ashnamehrotra <ashnamehrotra@gmail.com>
2025-06-17 11:55:09 -0400

    use PlatformKey
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- pkg/buildkit/buildkit.go
+++ pkg/buildkit/buildkit.go
@@ -239,23 +239,19 @@ func DiscoverPlatforms(manifestRef, reportDir, scanner string) ([]types.PatchPla
 		log.WithField("platforms", p2).Debug("Discovered platforms from report")
 
 		// include all platforms from original manifest, patching only those with reports
-		key := func(pl ispec.Platform) string {
-			return pl.OS + "/" + pl.Architecture + "/" + pl.Variant
-		}
-
 		reportSet := make(map[string]string, len(p2))
 		for _, pl := range p2 {
 			reportSet[PlatformKey(pl.Platform)] = pl.ReportFile
 		}
 
 		for _, pl := range p {
-			if rp, ok := reportSet[key(pl.Platform)]; ok {
+			if rp, ok := reportSet[PlatformKey(pl.Platform)]; ok {
 				// Platform has a report - will be patched
 				pl.ReportFile = rp
 				platforms = append(platforms, pl)
 			} else {
 				// Platform has no report - preserve original without patching
-				log.Debugf("No report found for platform %s, preserving original", key(pl.Platform))
+				log.Debugf("No report found for platform %s, preserving original", PlatformKey(pl.Platform))
 				pl.ReportFile = ""
 				platforms = append(platforms, pl)
 			}
