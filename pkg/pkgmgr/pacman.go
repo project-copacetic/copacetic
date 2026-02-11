f67bdd6c8876d6e73064bfec6215ba111e833ca7
PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>
2026-02-11 20:34:59 +0530

    feat: add upgradePackages function to pacman.go
    Signed-off-by: PARTHIV PRATIM SAIKIA <parthivsaikia985@gmail.com>

--- pkg/pkgmgr/pacman.go
+++ pkg/pkgmgr/pacman.go
@@ -3,14 +3,18 @@ package pkgmgr
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"errors"
 	"fmt"
 	"sort"
 	"strings"
 
+	"github.com/moby/buildkit/client/llb"
 	pacmanVer "github.com/parthivsaikia/go-pacman-version"
 	"github.com/project-copacetic/copacetic/pkg/buildkit"
+	"github.com/project-copacetic/copacetic/pkg/types"
 	"github.com/project-copacetic/copacetic/pkg/types/unversioned"
+	"github.com/project-copacetic/copacetic/pkg/utils"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -100,4 +104,89 @@ func validatePacmanPackageVersions(updates unversioned.UpdatePackages, cmp Versi
 	}
 
 	return errorPkgs, errors.Join(allErrors...)
+}
+
+func (pm *pacmanManager) upgradePackages(ctx context.Context, updates unversioned.UpdatePackages, ignoreErrors bool) (*llb.State, []byte, error) {
+	imageStateCurrent := pm.config.ImageState
+	if pm.config.PatchedConfigData != nil {
+		imageStateCurrent = pm.config.PatchedImageState
+	}
+
+	pacmanUpdated := imageStateCurrent.Run(
+		llb.Shlex("pacman -Sy"),
+		llb.WithProxy(utils.GetProxy()),
+		llb.IgnoreCache,
+		llb.WithCustomName("Updating package database"),
+	).Root()
+
+	if updates == nil {
+		const updatesAvailableMarker = "/updates.txt"
+		checkUpgradable := fmt.Sprintf(`sh -c 'if pacman -Qu > /dev/null 2 &1; then touch %s; fi'`, updatesAvailableMarker)
+		stateWithCheck := pacmanUpdated.Run(
+			llb.Shlex(checkUpgradable),
+			llb.WithCustomName("Checking for available updates"),
+		).Root()
+
+		_, err := buildkit.ExtractFileFromState(ctx, pm.config.Client, &stateWithCheck, updatesAvailableMarker)
+		if err != nil {
+			log.Info("No upgradable packages found for this image.")
+			return nil, nil, types.ErrNoUpdatesFound
+		}
+	}
+
+	var pacmanInstalled llb.State
+	var resultManifestBytes []byte
+	var err error
+	if updates != nil {
+		pkgStrings := []string{}
+		for _, u := range updates {
+			pkgStrings = append(pkgStrings, u.Name)
+		}
+		const pacmanInstallTemplate = `pacman -S --noconfirm %s`
+		installCmd := fmt.Sprintf(pacmanInstallTemplate, strings.Join(pkgStrings, " "))
+		pacmanInstalled.Run(
+			llb.Shlex(installCmd),
+			llb.WithProxy(utils.GetProxy()),
+			llb.WithCustomName(fmt.Sprintf("Upgrading %d security updates", len(pkgStrings))),
+		).Root()
+
+		const outputResultsTemplate = `sh -c 'pacman -Q %s > %s; if [[ $? -ne 0 ]]; then echo "WARN: pacman -Q returned $?"; fi'`
+		pkgs := strings.Trim(fmt.Sprintf("%s", pkgStrings), "[]")
+		outputResultsCmd := fmt.Sprintf(outputResultsTemplate, pkgs, resultManifest)
+		mkFolders := pacmanInstalled.File(llb.Mkdir(resultsPath, 0o744, llb.WithParents(true)))
+		resultDiff := mkFolders.Dir(resultsPath).Run(llb.Shlex(outputResultsCmd)).AddMount(resultsPath, llb.Scratch())
+
+		resultManifestBytes, err = buildkit.ExtractFileFromState(ctx, pm.config.Client, &resultDiff, resultManifest)
+		if err != nil {
+			return nil, nil, err
+		}
+
+	} else {
+		installCmd := `output=$(pacman -Su --noconfirm 2>&1); if [ $? -ne 0 ]; then echo "$output" >> error_log.txt; fi`
+		pacmanInstalled = pacmanUpdated.Run(
+			buildkit.Sh(installCmd),
+			llb.WithProxy(utils.GetProxy()),
+			llb.WithCustomName("Upgrading all packages"),
+		).Root()
+
+		if !ignoreErrors {
+			pacmanInstalled = pacmanUpdated.Run(
+				buildkit.Sh("if [ -s error_log.txt ]; then cat error_log.txt; exit 1; fi"),
+				llb.WithCustomName("Validating package updates"),
+			).Root()
+		}
+	}
+
+	if pm.config.PatchedConfigData != nil {
+		prevPatchDiff := llb.Diff(pm.config.ImageState, pm.config.PatchedImageState)
+		newPatchDiff := llb.Diff(pacmanUpdated, pacmanInstalled)
+		combinedPatch := llb.Merge([]llb.State{prevPatchDiff, newPatchDiff})
+		squashedPatch := llb.Scratch().File(llb.Copy(combinedPatch, "/", "/"))
+		completePatchMerge := llb.Merge([]llb.State{pm.config.ImageState, squashedPatch})
+		return &completePatchMerge, resultManifestBytes, nil
+	}
+
+	patchDiff := llb.Diff(pacmanUpdated, pacmanInstalled)
+	patchMerge := llb.Merge([]llb.State{pm.config.ImageState, patchDiff})
+	return &patchMerge, resultManifestBytes, nil
 }
