1d83dc918b4cb372ecc55a29cd41b4ff8fde9720
Sertaç Özercan <852750+sozercan@users.noreply.github.com>
2026-04-24 02:29:17 -0700

    fix: avoid masking package manager failures as no-updates (#1530)
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>
    Signed-off-by: robert-cronin <robert@robertcronin.com>
    Signed-off-by: robert-cronin <robert.owen.cronin@gmail.com>
    Co-authored-by: robert-cronin <robert@robertcronin.com>
    Co-authored-by: Robbie Cronin <robert.owen.cronin@gmail.com>

--- pkg/pkgmgr/dpkg.go
+++ pkg/pkgmgr/dpkg.go
@@ -359,8 +359,11 @@ func (dm *dpkgManager) installUpdates(ctx context.Context, updates unversioned.U
 			llb.WithCustomName("Checking for upgradable packages"),
 		).Root()
 
-		_, err := buildkit.ExtractFileFromState(ctx, dm.config.Client, &aptGetUpdated, updatesAvailableMarker)
+		_, err := buildkit.TryExtractFileFromState(ctx, dm.config.Client, &aptGetUpdated, updatesAvailableMarker)
 		if err != nil {
+			if !isMarkerMissingErr(err, updatesAvailableMarker) {
+				return nil, nil, fmt.Errorf("failed while checking for available apt updates: %w", err)
+			}
 			log.Info("No upgradable packages found for this image.")
 			return nil, nil, types.ErrNoUpdatesFound
 		}
